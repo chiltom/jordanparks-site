00dd2b6d6070c33c82f7f78726a2976e2d88d295
Tom Childress <thomas.childress02@gmail.com>
2024-05-29 17:27:06 -1000

    create: created navbar component and inserted into root layout.tsx

--- app/ui/navigation/nav-links.tsx
+++ app/ui/navigation/nav-links.tsx
@@ -32,24 +32,24 @@ const NavLinks = (): ReactElement => {
   const pathname: string = usePathname();
 
   return (
-    <>
+    <div className="flex flex-row">
       {links.map((link) => {
         return (
           <Link
             key={link.name}
             href={link.href}
             className={clsx(
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              "flex grow items-center justify-center gap-2 rounded-md p-1 text-sm font-medium text-white md:p-3 hover:text-blue-600",
               {
                 "bg-sky-100 text-blue-600": pathname === link.href,
               }
             )}
           >
-            <p className="hidden md:block">{link.name}</p>
+            <p>{link.name}</p>
           </Link>
         );
       })}
-    </>
+    </div>
   );
 };
 
--- app/ui/navigation/navbar.tsx
+++ app/ui/navigation/navbar.tsx
@@ -1,3 +1,20 @@
 import { ReactElement } from "react";
 import Link from "next/link";
+import NavLinks from "./nav-links";
 
+const Navbar = (): ReactElement => {
+  return (
+    <nav className="bg-gray-600 p-4">
+      <div className="container mx-auto flex justify-around items-center">
+        <div className="text-white text-xl font-bold">
+          <Link href="/">Image</Link>
+        </div>
+        <div className="space-x-4">
+          <NavLinks />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
