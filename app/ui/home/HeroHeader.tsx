e4729e59b8fc9e68afe3f06555cfd5c5cde7de58
Tom Childress <thomas.childress02@gmail.com>
2024-06-06 10:08:52 -1000

    update: updated home page styles

--- app/ui/home/HeroHeader.tsx
+++ app/ui/home/HeroHeader.tsx
@@ -2,9 +2,9 @@ import { ReactElement } from "react";
 
 const HeroHeader: React.FC = (): ReactElement => {
   return (
-    <header className="bg-accent text-buttonText text-center rounded-b-lg py-20 px-4 shadow-neonPink mx-2">
-      <h1 className="text-5xl font-bold">Discover Unique, Amazing Music</h1>
-      <p className="mt-4 text-lg">A hero image with a subtle tagline</p>
+    <header className="text-center py-20 px-4 shadow-lg">
+      <h1 className="text-5xl font-bold text-white">Discover Unique, Amazing Music</h1>
+      <p className="mt-4 text-lg text-gray-300">A hero image with a subtle tagline</p>
     </header>
   );
 };
