474879b78d8c7748bc7afd94fe7729b99a251842
Tom Childress <thomas.childress02@gmail.com>
2024-06-06 10:34:06 -1000

    updated navbar with toggle

--- app/ui/navigation/SiteFooter.tsx
+++ app/ui/navigation/SiteFooter.tsx
@@ -2,8 +2,8 @@ import { ReactElement } from "react";
 
 const Footer: React.FC = (): ReactElement => {
   return (
-    <footer className="bg-navbarBackground border-t border-secondary text-center p-4">
-      <p className="text-textSecondary">
+    <footer className="text-center p-4">
+      <p className="text-white">
         &copy; 2024 Jordan Parks. All rights reserved.
       </p>
     </footer>
--- app/ui/navigation/SiteNavbar.tsx
+++ app/ui/navigation/SiteNavbar.tsx
@@ -1,18 +1,41 @@
-import { ReactElement } from "react";
+"use client";
+
+import { ReactElement, useState } from "react";
 import Link from "next/link";
 import NavLinks from "./NavLinks";
+import Image from "next/image";
+import Brand from "@/public/brand.webp";
+import ToggleIcon from "@/public/flower-icon.png"; // Update with the correct path to your image
 
 const Navbar = (): ReactElement => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="p-4">
-      <div className="container mx-auto flex justify-around items-center">
+      <div className="container mx-auto flex justify-between items-center">
         <div className="text-textPrimary text-xl font-bold md:text-2xl">
-          <Link href="/">Image</Link>
+          <Link href="/">
+            <Image src={Brand} alt="Jordan Parks" width={100} height={50} />
+          </Link>
         </div>
-        <div className="space-x-4">
-          <NavLinks />
+        <div className="hidden md:flex space-x-4">
+          <NavLinks isMobile={false} />
+        </div>
+        <div className="md:hidden">
+          <button onClick={handleToggle} className="focus:outline-none">
+            <Image src={ToggleIcon} alt="Menu Toggle" width={30} height={30} />
+          </button>
         </div>
       </div>
+      {isOpen && (
+        <div className="md:hidden">
+          <NavLinks isMobile={true} />
+        </div>
+      )}
     </nav>
   );
 };
