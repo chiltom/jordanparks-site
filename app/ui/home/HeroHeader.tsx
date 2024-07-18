bd438d8c58b4f57aed635d8818c5151d4aafd783
chiltom <thomas.childress02@gmail.com>
2024-07-18 07:06:03 -1000

    update: updated structure of home page more to where Jordan wanted it

--- app/ui/home/HeroHeader.tsx
+++ app/ui/home/HeroHeader.tsx
@@ -2,9 +2,13 @@ import { ReactElement } from "react";
 
 const HeroHeader: React.FC = (): ReactElement => {
   return (
-    <header className="text-center py-20 px-4 shadow-lg">
-      <h1 className="text-5xl font-bold text-white">Discover Unique, Amazing Music</h1>
-      <p className="mt-4 text-lg text-gray-300">A hero image with a subtle tagline</p>
+    <header className="text-center flex flex-col pt-72 w-full h-full">
+      <h1 className="text-5xl font-bold text-white">
+        Discover Unique, Amazing Music
+      </h1>
+      <p className="mt-4 text-lg text-gray-300">
+        A hero image with a subtle tagline
+      </p>
     </header>
   );
 };
