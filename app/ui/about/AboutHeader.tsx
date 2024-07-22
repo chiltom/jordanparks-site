3f620053f4a2e5ee2dd5aa1215135d112e8d1394
Tom Childress <thomas.childress02@gmail.com>
2024-07-21 21:39:42 -1000

    update: updated AutoBiography component with Jordan's autobiography, added photos to public folder as well

--- app/ui/about/AboutHeader.tsx
+++ app/ui/about/AboutHeader.tsx
@@ -3,9 +3,8 @@ import { ReactElement } from "react";
 const AboutHeader: React.FC = (): ReactElement => {
   return (
     <header className="text-center flex flex-col pt-72 w-full h-full">
-      <h1 className="text-5xl font-bold text-white">About Jordan</h1>
+      <h1 className="text-5xl font-bold text-white">Jordan</h1>
       <p className="mt-4 text-lg text-gray-300">Another picture of Jordan</p>
-      <p className="mt-4 text-lg text-gray-300">Brief background description</p>
     </header>
   );
 };
